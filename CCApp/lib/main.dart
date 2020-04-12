f19084efc067e2d666d32048f720a3519cb13121
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-04-12 23:32:58 +0530

    second commit
    :wq
    q!

--- CCApp/lib/main.dart
+++ CCApp/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo.',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page.'),
     );
   }
 }
