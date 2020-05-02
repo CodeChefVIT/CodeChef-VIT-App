0742c9c95f6f17a262235573a9ba0e90715d9dbd
Rishav <jainrv0510@gmail.com>
2020-05-02 13:08:55 +0530

    backend

---
+++ Backend/src/routers/meeting.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const Task = require('../models/meeting')
+const router = new express.Router()
+
+router.post('/create', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/get', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
+module.exports = router
---
+++ Backend/src/routers/user.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+router.post('/register', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+router.get('/profile', auth, async (req, res) => {
+    res.send(req.user)
+})
+module.exports = router
