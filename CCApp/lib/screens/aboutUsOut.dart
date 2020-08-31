938f560efbb1451248eb4681317357f3ff6fdc67
DananjayM <dananjaymurugesh2001@gmail.com>
2020-08-31 08:06:15 +0530

    Added events and link to codechef-vit website and instagram page

--- CCApp/lib/screens/aboutUsOut.dart
+++ CCApp/lib/screens/aboutUsOut.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OutsideAboutUs extends StatelessWidget {
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+  int i;
   List<String> image = [
     'kunal.jpg',
     'rajat.jpg',
@@ -70,6 +80,31 @@ class OutsideAboutUs extends StatelessWidget {
     '+91 6394577290',
     '+91 9315673060',
   ];
+  List<String> eventimage20=[
+    'devsoc20.jpg',
+  ];
+  List<String> eventimage19=[
+    'devsoc19.jpg',
+    'webapps.jpeg',
+    'cook.jpeg',
+    'insight.jpeg',
+    'game.jpeg',
+    'server.jpeg',
+    'blitz.jpeg',
+  ];
+  List<String> eventimage18=[
+    'blockchain.jpg',
+    'data_analytics.jpg',
+    'error404.jpg',
+    'codecombat.jpeg',
+    'who-breaks-first.jpeg',
+    'seriesbuff.jpeg',
+  ];
+  List<String> eventimage17=[
+    'insight17.jpeg',
+    'bigdata.jpeg',
+    'cookoff3.jpeg'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -111,20 +146,43 @@ class OutsideAboutUs extends StatelessWidget {
           Container(
             padding: EdgeInsets.only(left: 24, right: 24),
             alignment: Alignment.centerLeft,
-            child: Text(
-              'Codechef-VIT is one of the prestigious technical chapters of VIT Vellore. '
-              'Our aim is to educate and enable the youth who will lead in various innovative endeavours. '
-              'We hope that together, each one of us may contribute to the chapter\'s countless accolades and leave a lasting mark. '
-              'Check out our website at https://www.codechefvit.com/',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-                fontFamily: 'SFProDisplay',
+            child: RichText(
+              text: TextSpan(
+                style: TextStyle(color: Colors.black, fontSize: 16.0),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Codechef-VIT is one of the prestigious technical chapters of VIT Vellore. '
+                          'Our aim is to educate and enable the youth who will lead in various innovative endeavours. '
+                          'We hope that together, each one of us may contribute to the chapter\'s countless accolades and leave a lasting mark. '
+                          '\nCheck out our '
+                  ),
+                  TextSpan(
+                      text: 'website.',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          const url = 'https://codechefvit.com';
+                          launchURL(url);
+                        }),
+                  TextSpan(
+                      text: '\nFollow us on '
+                  ),
+                  TextSpan(
+                      text: 'instagram ',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          const url = 'https://www.instagram.com/codechefvit';
+                          launchURL(url);
+                        }),
+                  TextSpan(
+                      text: 'to stay updated on our activities.'
+                  ),
+                ],
               ),
             ),
           ),
-          SizedBox(height: 30),
+          SizedBox(height: 20),
           Container(
             padding: EdgeInsets.only(left: 24),
             alignment: Alignment.centerLeft,
@@ -210,6 +268,142 @@ class OutsideAboutUs extends StatelessWidget {
                   );
                 }),
           ),
+          SizedBox(height:20),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Events',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                fontFamily: 'SFProDisplay',
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '2020',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                fontFamily: 'SFProDisplay',
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          for(i=0;i<eventimage20.length;i++)
+            Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: InkWell(
+                onTap: (){
+                  print("tapped");
+                },
+                child: Image.asset(
+                  'assets/images/Events/${eventimage20[i]}',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+            ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '2019',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                fontFamily: 'SFProDisplay',
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          for(i=0;i<eventimage19.length;i++)
+            Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Image.asset(
+                'assets/images/Events/${eventimage19[i]}',
+                fit: BoxFit.fill,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+            ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '2018',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                fontFamily: 'SFProDisplay',
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          for(i=0;i<eventimage18.length;i++)
+            Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Image.asset(
+                'assets/images/Events/${eventimage18[i]}',
+                fit: BoxFit.fill,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+            ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '2017',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                fontFamily: 'SFProDisplay',
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          for(i=0;i<eventimage17.length;i++)
+            Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Image.asset(
+                'assets/images/Events/${eventimage17[i]}',
+                fit: BoxFit.fill,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+            ),
+          SizedBox(height: 10,),
         ]),
       ),
     );
